af05d75704b5f065b7db7be392c04a569e31f96d
Willem Serruys <serruys.willem@hotmail.com>
2025-10-17 09:09:07 +0200

    chore: added helpers.rs in cli crate

---
+++ bridge-cli/src/helpers.rs
@@ -0,0 +1,35 @@
+use bridge_core::OffsetSnapshot;
+use comfy_table::Table;
+use inquire::Text;
+
+pub fn ask_for_confirmation(offset_snapshot: &OffsetSnapshot) -> bool {
+    let mut table = Table::new();
+
+    table.set_header(vec![
+        "Consumer Group",
+        "Topic",
+        "Partition",
+        "Target Offset",
+    ]);
+
+    for intermediate_result_item in offset_snapshot {
+        table.add_row(vec![
+            intermediate_result_item.consumer_group.clone(),
+            intermediate_result_item.topic.clone(),
+            intermediate_result_item.partition.to_string(),
+            intermediate_result_item.offset.to_string(),
+        ]);
+    }
+    println!("{table}");
+    let prompt = Text::new("The offsets above will be applied. Are you sure? (Y/n)").prompt();
+    matches!(prompt, Ok(value) if value == "Y")
+}
+
+pub fn print_offset_snapshot(offset_snapshot: &OffsetSnapshot) {
+    for element in offset_snapshot {
+        println!(
+            "{},{},{},{}",
+            element.consumer_group, element.topic, element.partition, element.offset
+        );
+    }
+}
--- bridge-cli/src/main.rs
+++ bridge-cli/src/main.rs
@@ -1,11 +1,14 @@
 use args::{Args, Commands};
-use bridge_core::{BridgeClient, KafkaBridgeClient, OffsetSnapshot, errors::BridgeError, helpers};
+use bridge_core::{
+    BridgeClient, KafkaBridgeClient, OffsetSnapshot, errors::BridgeError,
+    helpers::get_offset_records,
+};
 use clap::Parser;
-use comfy_table::Table;
-use inquire::Text;
+use helpers::{ask_for_confirmation, print_offset_snapshot};
 use log::trace;
 
 mod args;
+mod helpers;
 
 #[tokio::main]
 async fn main() -> Result<(), BridgeError> {
@@ -39,7 +42,7 @@ async fn main() -> Result<(), BridgeError> {
         } => {
             let topics = &kafka_connection.topics.clone();
             let client: KafkaBridgeClient = kafka_connection.into();
-            let offset_snapshot = helpers::get_offset_records(&input)?;
+            let offset_snapshot = get_offset_records(&input)?;
             let result = client
                 .calculate_target_offsets(offset_header.as_str(), topics, offset_snapshot)
                 .await?;
@@ -53,7 +56,7 @@ async fn main() -> Result<(), BridgeError> {
         } => {
             let topics = &kafka_connection.topics.clone();
             let client: KafkaBridgeClient = kafka_connection.into();
-            let offset_snapshot = helpers::get_offset_records(&input)?;
+            let offset_snapshot = get_offset_records(&input)?;
 
             let confirmation_clojure = match skip_confirmation {
                 true => |_: &OffsetSnapshot| true,
@@ -67,36 +70,4 @@ async fn main() -> Result<(), BridgeError> {
     };
     trace!("Execution finished.");
     result
-}
-
-fn ask_for_confirmation(offset_snapshot: &OffsetSnapshot) -> bool {
-    let mut table = Table::new();
-
-    table.set_header(vec![
-        "Consumer Group",
-        "Topic",
-        "Partition",
-        "Target Offset",
-    ]);
-
-    for intermediate_result_item in offset_snapshot {
-        table.add_row(vec![
-            intermediate_result_item.consumer_group.clone(),
-            intermediate_result_item.topic.clone(),
-            intermediate_result_item.partition.to_string(),
-            intermediate_result_item.offset.to_string(),
-        ]);
-    }
-    println!("{table}");
-    let prompt = Text::new("The offsets above will be applied. Are you sure? (Y/n)").prompt();
-    matches!(prompt, Ok(value) if value == "Y")
-}
-
-fn print_offset_snapshot(offset_snapshot: &OffsetSnapshot) {
-    for element in offset_snapshot {
-        println!(
-            "{},{},{},{}",
-            element.consumer_group, element.topic, element.partition, element.offset
-        );
-    }
 }
