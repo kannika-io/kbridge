8d725b9fbcbb8cff04e8710cf4450dd1f763bc5f
Tom Verelst <664136+tomverelst@users.noreply.github.com>
2026-01-14 13:53:38 +0100

    fix: correct filter in apply command

--- bridge-core/src/commands/apply_target_offsets/mod.rs
+++ bridge-core/src/commands/apply_target_offsets/mod.rs
@@ -1,7 +1,7 @@
+use crate::OffsetSnapshot;
 use crate::commands::apply_target_offsets::errors::ApplyOffsetsError;
 use crate::commands::apply_target_offsets::export::apply_target_offsets;
 use crate::kafka::client_config::ConfigBuilder;
-use crate::{ApplicationRecord, ConsumerGroup, OffsetSnapshot};
 use rdkafka::ClientConfig;
 use std::collections::HashMap;
 use tracing::trace;
@@ -19,33 +19,21 @@ pub async fn execute(
     trace!("apply target offsets");
 
     let topics = topics.into_iter().collect::<Vec<String>>();
+    let filtered = offset_snapshot.filter_by_topics(&topics);
 
-    let mut exporter_base_config = ClientConfig::new()
-        .set_properties(properties)
-        .set_bootstrap_server(bootstrap_server)
-        .set_reset_from_beginning()
-        .disable_auto_commit();
-
-    let mut mapped_intermediary_result: HashMap<ConsumerGroup, Vec<ApplicationRecord>> =
-        HashMap::new();
-
-    offset_snapshot
-        .iter()
-        .filter(|r| topics.contains(&r.topic))
-        .for_each(|item| {
-            let value = (item.topic.to_string(), item.partition, item.offset);
-            mapped_intermediary_result
-                .entry(item.consumer_group.clone())
-                .and_modify(|list| list.push(value.clone()))
-                .or_insert(vec![value.clone()]);
-        });
-
-    if mapped_intermediary_result.is_empty() {
+    if filtered.is_empty() {
         return Err(ApplyOffsetsError::NoOffsetsToApply);
     }
 
     if !dry_run {
-        apply_target_offsets(&mut exporter_base_config, &mapped_intermediary_result).await?;
+        let mut config = ClientConfig::new()
+            .set_properties(properties)
+            .set_bootstrap_server(bootstrap_server)
+            .set_reset_from_beginning()
+            .disable_auto_commit();
+
+        apply_target_offsets(&mut config, &filtered).await?;
     }
+
     Ok(())
 }
